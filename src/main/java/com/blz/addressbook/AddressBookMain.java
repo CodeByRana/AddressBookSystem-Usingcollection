9caf4f71482e8c8525130ae9ce3cfc0a0ff0be41
deepakphpweb <dipakrana2760@gmail.com>
2021-12-21 22:58:31 +0530

    Added : Create a new contact to address book

--- src/main/java/com/blz/addressbook/AddressBookMain.java
+++ src/main/java/com/blz/addressbook/AddressBookMain.java
@@ -1,7 +1,71 @@
 package com.blz.addressbook;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book System Program");
+
+        firstActions();
     }
+    static String firstName;
+    static String lastName;
+    static String addressName;
+    static String cityName;
+    static String stateName;
+    static String phoneNumber;
+    static String emailId;
+    static String search = null;
+    static String fetchData = null;
+    static public int choice = 0;
+    static Scanner input = new Scanner (System.in);
+    static ContactInformation contact;
+    static ArrayList<String> information = new ArrayList<String>();
+
+    //put here choice function like which activity you want to perform
+    public static void firstActions() {
+        System.out.println("Address Book Menu: What would you like to do? 1) Add data. 2) Close.");
+        choice = input.nextInt();
+        switch (choice) {
+            case 1:
+                inputData();
+            case 2:
+                System.out.println("Application terminated.");
+                System.exit(0);
+            default:
+                System.out.println("Please compile again.");
+                break;
+        }
+    }
+        //Add data by collection library
+        public static void inputData()
+        {
+            information = new ArrayList<String>();
+            contact = new ContactInformation(firstName, lastName,addressName,cityName,stateName,phoneNumber,emailId,information);
+            System.out.println("What is your first name?");
+            contact.setFirstName(input.next());
+            information.add(contact.getFirstName());
+            System.out.println("What is your last name?");
+            contact.setLastName(input.next());
+            information.add(contact.getLastName());
+            System.out.println("What is your address?");
+            contact.setAddressName(input.next());
+            information.add(contact.getAddressName());
+            System.out.println("What is your city?");
+            contact.setCityName(input.next());
+            information.add(contact.getCityName());
+            System.out.println("What is your state?");
+            contact.setStateName(input.next());
+            information.add(contact.getStateName());
+            System.out.println("What is your phone number?");
+            contact.setPhoneNumber(input.next());
+            information.add(contact.getPhoneNumber());
+            System.out.println("What is your email id?");
+            contact.setEmailId(input.next());
+            information.add(contact.getEmailId());
+            System.out.println("Saved.");
+            System.out.println("What would you like to do next?");
+            firstActions();
+        }
 }
+
